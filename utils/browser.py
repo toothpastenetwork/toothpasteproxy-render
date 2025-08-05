e5487d18e87728094a4457df06589325239a099d
AverageToothpasteEnjoyer <toothpasteisverygood@gmail.com>
2025-08-05 11:47:02 -0400

    Create browser.py

---
+++ utils/browser.py
@@ -0,0 +1,20 @@
+from playwright.async_api import async_playwright
+
+_browser = None
+_playwright = None
+
+async def get_browser():
+    global _browser, _playwright
+    if _browser is None:
+        _playwright = await async_playwright().start()
+        _browser = await _playwright.chromium.launch(headless=True)
+    return _browser
+
+async def shutdown_browser():
+    global _browser, _playwright
+    if _browser:
+        await _browser.close()
+        _browser = None
+    if _playwright:
+        await _playwright.stop()
+        _playwright = None
